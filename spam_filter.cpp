f775be43331a0b2c831c909240582ee47092c57e
muyaho-universe <kimdaesk@gmail.com>
2022-11-20 02:26:15 +0900

    working on csv

---
+++ spam_filter.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+#include <sstream>
+#include <istream>
+
+using namespace std;
+
+vector<string> csv_read_row(istream &file, char delimiter);
+
+int main(int argc, char *argv[]){
+    ifstream file("c:\\");
+    // C:\Users\kimda\OneDrive\바탕 화면\학교자료\2022-2\이산수학\DM202202_HW6_spamfilter\DM202202_HW6_spamfilter\csv\test
+    // C:\Users\kimda\OneDrive\바탕 화면\학교자료\2022-2\이산수학\DM202202_HW6_spamfilter\DM202202_HW6_spamfilter\csv\train
+}
