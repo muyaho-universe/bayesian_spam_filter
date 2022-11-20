7a3b14deba40818061d0f36488afd16c15a6ef30
muyaho-universe <kimdaesk@gmail.com>
2022-11-21 00:45:05 +0900

    csv file is read

--- spam_filter.cpp
+++ spam_filter.cpp
@@ -10,7 +10,63 @@ using namespace std;
 vector<string> csv_read_row(istream &file, char delimiter);
 
 int main(int argc, char *argv[]){
-    ifstream file("c:\\");
-    // C:\Users\kimda\OneDrive\바탕 화면\학교자료\2022-2\이산수학\DM202202_HW6_spamfilter\DM202202_HW6_spamfilter\csv\test
-    // C:\Users\kimda\OneDrive\바탕 화면\학교자료\2022-2\이산수학\DM202202_HW6_spamfilter\DM202202_HW6_spamfilter\csv\train
+    ifstream train_file_ham(".\\csv\\train\\dataset_ham_train100.csv");
+    ifstream train_file_spam(".\\csv\\train\\dataset_spam_train100.csv");
+    
+    if(train_file_ham.fail()){
+        return (cout << "햄 파일 없음" << endl) && 0;
+    }
+    
+    if(train_file_spam.fail()){
+        return (cout << "스팸 파일 없음" << endl) && 0;
+    }
+
+    while (train_file_ham.good()){
+        vector<string> row = csv_read_row(train_file_ham, ',');
+        if(!row[0].find("#")) continue;
+        else{
+            for (int i = 0, leng = row.size() - 2; i < leng; i++){
+                cout << "[" << row[i] << "]" << "\t";
+            }
+            // cout << endl;
+        }
+    }
+    train_file_ham.close();
+    train_file_spam.close();
+    return 0;
+}
+
+vector<string> csv_read_row(istream &file, char delimiter){
+    stringstream ss;
+    bool inquotes = false;
+    vector<string> row;
+
+    while (file.good()){
+        char c = file.get();
+        
+        if(!inquotes && c == '""'){
+            inquotes =true;
+        }
+        else if (inquotes && c == '""'){
+            if( file.peek() == '"'){
+                ss << (char) file.get();
+            }
+            else{
+                inquotes = false;
+            }
+        }
+        else if (!inquotes && c == delimiter){
+           row.push_back(ss.str());
+           ss.str("");
+        }
+        else if (!inquotes && (c =='\r' || c == '\n')){
+            if(file.peek() =='\n') {file.get();}
+            row.push_back(ss.str());
+            return row;
+        }
+        else{
+            ss << c;
+        }
+    }
+    
 }
